e276378cc1eb0549d7a186526b186b7f1ebd4de3
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-11-21 20:38:43 +0100

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,1 +1,6 @@
-
+DROP TABLE IF EXISTS `rc_universal`;
+CREATE TABLE `rc_universal` (
+  `count` int(11) NOT NULL,
+  `plate` varchar(100),
+  `state` varchar(100)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
